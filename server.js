3a4ed307fd81b181643696173edb4f9d60cf375c
sadieb23 <sadiebalk3@gmail.com>
2024-01-30 16:30:11 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
-// Budget API
-
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
+// Function to read budget data from the JSON file
+const readBudgetData = () => {
+    const jsonFilePath = path.join(__dirname, 'budget-data.json');
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
+        return JSON.parse(jsonData);
+    } catch (error) {
+        console.error(`Error reading JSON file: ${error.message}`);
+        return {};
+    }
 };
 
-
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    const budgetData = readBudgetData();
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
