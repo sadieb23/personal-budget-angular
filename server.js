1a1f943a939cb31056603489b3758e306fc2f9d1
sadieb23 <sadiebalk3@gmail.com>
2024-02-02 11:34:00 -0500

    Added D3JS chart

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use('/', express.static('public'));
 
 // Function to read budget data from the JSON file
